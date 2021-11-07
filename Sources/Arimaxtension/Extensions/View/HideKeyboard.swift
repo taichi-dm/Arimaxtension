c1529976a74c47fc615d5091478a5f68ba60ec68
taichi <positive703@gmail.com>
2021-11-07 22:58:31 +0900

    removed unnecessary keyword "public."

--- Sources/Arimaxtension/Extensions/View/HideKeyboard.swift
+++ Sources/Arimaxtension/Extensions/View/HideKeyboard.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 #if canImport(UIKit)
 public extension View {
-    public func hideKeyboard() {
+    func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
