871c026e5804a9512821c380ba6598458569f68e
taichi <positive703@gmail.com>
2021-11-09 22:16:12 +0900

    [feat] added hideKeyboard method.

---
+++ Sources/Arimaxtension/Extensions/UIApplication/HideKeyboard.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Taichi Arima on 2021/11/09.
+//
+
+import Foundation
+import UIKit
+
+public extension UIApplication {
+    func hideKeyboard() {
+        DispatchQueue.main.async {
+            self.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        }
+    }
+}
+
