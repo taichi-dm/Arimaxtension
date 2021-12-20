6a2038bed41dde018bea34abe4093b7c838898f1
taichi <positive703@gmail.com>
2021-12-21 00:45:42 +0900

    [feat] Added View.Redacted(showPlaceholder:) method!!!

---
+++ Sources/Arimaxtension/Extensions/View/View+Redacted.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Taichi Arima on 2021/12/21.
+//
+
+import SwiftUI
+
+public extension View {
+    @ViewBuilder
+    public func redacted(showPlaceholder: Bool) -> some View {
+        if showPlaceholder {
+            self
+                .redacted(reason: .placeholder)
+                .modifier(EaseInOutAnimation())
+        } else {
+            self
+                .unredacted()
+        }
+    }
+}
+
+fileprivate struct EaseInOutAnimation: ViewModifier {
+    @State private var contentOpacity = 1.0
+    
+    private let easeInOutAnimation: Animation = Animation.easeInOut(duration: 2).repeatForever(autoreverses: true)
+    
+    func body(content: Content) -> some View {
+        content
+            .opacity(contentOpacity)
+            .onAppear(perform: { withAnimation(easeInOutAnimation) { contentOpacity = 0.3 } })
+    }
+}
+
