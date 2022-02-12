f089a9582cee5309208d0e6f34119ed19995e70c
youandtaichi <positive703@gmail.com>
2022-02-12 16:43:11 +0900

    added not

---
+++ Sources/Arimaxtension/not.swift
@@ -0,0 +1,21 @@
+//
+//  not.swift
+//
+//
+//  Created by 有馬大智 on 2022/02/12.
+//
+
+import Foundation
+
+/// from: https://twitter.com/greg_ellis/status/1492160811220881413?s=20&t=Z9HqK7tmq8wP3Q9IRUqAYw
+///
+///
+/// usage: to avoid use '!' for better redability
+///     let validEntry = false
+///     if not(validEntry) {
+///         print("Your entry is invalid.")
+///     }
+///
+public let not = (!)
+
+
