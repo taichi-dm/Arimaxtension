7cfee31105205e12e0cc4f75767d4081136a6b60
youandtaichi <positive703@gmail.com>
2022-02-19 10:29:47 +0900

    Added 'public' access control to Color.isDark

--- Sources/Arimaxtension/Extensions/SwiftUI/Color/Color+isDark.swift
+++ Sources/Arimaxtension/Extensions/SwiftUI/Color/Color+isDark.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension Color {
 
-    var isDark: Bool {
-        var (r, g, b, a): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+    public var isDark: Bool {
+        var (r, g, b, a):   (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
         UIColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
         let lum = 0.2126 * r + 0.7156 * g + 0.0722 * b
         return lum < 0.50
